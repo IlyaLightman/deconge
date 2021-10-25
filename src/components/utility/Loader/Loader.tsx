a2f8a09e469a89353894a3fd12b180ed660f1b80
Ilya Zaytsev <ilya@zaytsev.xyz>
2021-10-25 06:41:59 +0300

    styled-components loader

--- src/components/utility/Loader/Loader.tsx
+++ src/components/utility/Loader/Loader.tsx
@@ -1,11 +1,55 @@
 import React from 'react'
-import './Loader.css'
+import styled, { css, keyframes } from 'styled-components'
 
-const Loader: React.FC = () => <div className="lds-ring">
+const anim = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`
+
+const LoaderStyle = styled.div`
+  ${ css`
+    display: inline-block;
+    position: relative;
+    width: 80px;
+    height: 80px;
+
+    div {
+      box-sizing: border-box;
+      display: block;
+      position: absolute;
+      width: 64px;
+      height: 64px;
+      margin: 8px;
+      border: 8px solid #fff;
+      border-radius: 50%;
+      animation: ${anim} 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+      border-color: #fff transparent transparent transparent;
+    }
+
+    &:nth-child(1) {
+      animation-delay: -0.45s;
+    }
+
+    &:nth-child(2) {
+      animation-delay: -0.3s;
+    }
+
+    &:nth-child(3) {
+      animation-delay: -0.15s;
+    }
+  ;
+  ` }
+`
+
+const Loader: React.FC = () => <LoaderStyle>
     <div/>
     <div/>
     <div/>
     <div/>
-</div>
+</LoaderStyle>
 
 export default Loader
